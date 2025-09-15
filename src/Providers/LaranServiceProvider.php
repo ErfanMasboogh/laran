9396df9f5359aa825b711bfb7cabe19171e1e045
ErfanMasboogh <erfaanmasbogh@gmail.com>
2025-09-15 13:50:33 +0330

    feat(LaranServiceProvider): init

---
+++ src/Providers/LaranServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ErfanMasboogh\Laran\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaranServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
