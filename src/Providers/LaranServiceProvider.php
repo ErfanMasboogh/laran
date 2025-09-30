2ac4bcf2d340a62626f146c9047c925b8d6a6c22
ErfanMasboogh <erfaanmasbogh@gmail.com>
2025-09-30 10:58:12 +0330

    feat(Console): add CreateDatatableCommand for datatable creation

--- src/Providers/LaranServiceProvider.php
+++ src/Providers/LaranServiceProvider.php
@@ -5,6 +5,7 @@ namespace ErfanMasboogh\Laran\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
 use ErfanMasboogh\Laran\Rule\ValidMobile;
+use ErfanMasboogh\Laran\Console\Commands\CreateDatatableCommand;
 
 class LaranServiceProvider extends ServiceProvider
 {
@@ -21,28 +22,35 @@ class LaranServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Helpers
         require_once __DIR__ . '/../Helpers/Functions.php';
 
+        
+        // Loads and Merges
         $this->loadTranslationsFrom(__DIR__ . '/../../lang');
         $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'laran');
         $this->mergeConfigFrom(__DIR__ . '/../../config/laran.php', 'laran');
+        $this->mergeConfigFrom(__DIR__ . '/../../config/laranMenu.php', 'laranMenu');
 
 
+        // Publishes
         $this->publishes([
             __DIR__ . '/../../dist' => public_path('vendor/laran'),
         ], 'laran-assets');
         $this->publishes([
             __DIR__ . '/../../database/seeders' => database_path('seeders'),
-        ]);        
-        $this->publishes([
-            __DIR__ . '/../../config' => config_path(),
         ]);
 
-
+        // Auth
         $laranAuth = config('laran.auth');
         config(['auth.guards.manager' => $laranAuth['guards']['manager']]);
         config(['auth.providers.managers' => $laranAuth['providers']['managers']]);
+        
+        // Commands
+        $this->commands([
+            CreateDatatableCommand::class,
+        ]);
     }
 }
