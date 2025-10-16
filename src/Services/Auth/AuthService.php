fa5f916a2713456fe123a629a46c7667c0fa422d
ErfanMasboogh <erfaanmasbogh@gmail.com>
2025-10-16 16:58:00 +0330

    feat(AuthService): init and register method

---
+++ src/Services/Auth/AuthService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace ErfanMasboogh\Laran\Services\Auth;
+
+use App\Services\Sms\SmsService;
+use ErfanMasboogh\Laran\Http\Controllers\Api\Traits\HasApiResponse;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
+use Random\RandomException;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class AuthService
+{
+    use HasApiResponse;
+
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws RandomException
+     */
+    public function register(array $data)
+    {
+        $cachedData = Cache::get($data['mobile']) ?? [];
+        $otpConfig = config('app.otp');
+        $now = time();
+        $cacheTime = now()->addMinutes(config('app.otp.userInfoCacheTime'));
+
+        if ($cachedData && $cachedData['tryLimit'] >= $otpConfig['tryLimit']) {
+            if (!isset($cachedData['limitReachedTime'])) {
+                $cachedData['limitReachedTime'] = $now;
+                Cache::put($data['mobile'], $cachedData, $cacheTime);
+            }
+
+            if ($cachedData['limitReachedTime'] + $otpConfig['restrictTime'] < $now) {
+                $cachedData['tryLimit'] = 0;
+                unset($cachedData['limitReachedTime']);
+                Cache::put($data['mobile'], $cachedData, $cacheTime);
+            } else {
+                $remainedTime = $cachedData['limitReachedTime'] + $otpConfig['restrictTime'] - $now;
+                $remainedTime = ceil($remainedTime / 60);
+
+                throw new HttpResponseException(
+                    $this->error(lt('Register try limit error', ['minutes' => $remainedTime]),
+                        ResponseAlias::HTTP_TOO_MANY_REQUESTS
+                    )
+                );
+            }
+        } elseif ($cachedData && $now - $cachedData['lastSentTime'] <= $otpConfig['resendCoolDown']) {
+            $remainedTime = $otpConfig['resendCoolDown'] - ($now - $cachedData['lastSentTime']);
+            throw new HttpResponseException(
+                $this->error(lt('Register cool down error', ['seconds' => $remainedTime]),
+                    ResponseAlias::HTTP_TOO_MANY_REQUESTS)
+            );
+        }
+
+        $cachedData['lastSentTime'] = $now;
+        $cachedData['tryLimit'] = ($cachedData['tryLimit'] ?? 0) + 1;
+        $cachedData['otpCode'] = random_int(10000, 99999);
+        $cachedData['password'] = Hash::make($data['password']);
+
+        Cache::put($data['mobile'], $cachedData, $cacheTime);
+
+        $smsService = new SmsService();
+        return $smsService->sendOtp(
+            $data['mobile'],
+            $cachedData['otpCode'],
+            config('app.otp.' . config('app.smsService') . '.templateID')
+        );
+    }
+}
